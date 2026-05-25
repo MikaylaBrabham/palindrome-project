8269691efb16aea5ab8a5883475e6c79c6072323
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 01:45:22 -0500

    added palindome logic

--- src/main/java/com/pluralsight/Palindromes.java
+++ src/main/java/com/pluralsight/Palindromes.java
@@ -1,6 +1,25 @@
 package com.pluralsight;
 
 public class Palindromes {
-    float palindromes;
+    public static void main(String[] args) {
+
+        //add initial value
+        float palindromes = 0;
+
+        //create nested for loop
+        for (float i = 100; i <= 999; i++) {
+
+            for (float j = 100; j <= 999; j++) {
+
+                //add logic for 2 numbers three digits
+                float product = i * j;
+
+
+
+            }
+        }
+
+
+    }
 
 }
